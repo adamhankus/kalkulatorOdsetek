64fbf763218d3cbd1d6d22932e5ed024d368368d
Unknown <adam.hankus@gmail.com>
2017-04-08 12:51:53 +0200

    first commit

---
+++ js/kalkulator.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function oblicz() {
+  var wplata = parseInt(document.getElementById("wplata").value, 16);
+  var iloscLat = parseInt(document.getElementById("ilosc-lat").value, 8);
+  console.log(wplata);
+  console.log(iloscLat);
+  
+}
+
+var elementParent = document.getElementById("wplata").parentNode;
